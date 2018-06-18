b9dd58bbd4d3ec6cc16a7a47c74b410bacea4fb0
DominikDa <dominikda@advaoptical.com>
2018-06-18 20:05:01 +0200

    refactor

--- src/test/scala/compiler/ParserSpec.scala
+++ src/test/scala/compiler/ParserSpec.scala
@@ -5,30 +5,30 @@ import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class ParserTreeSpec extends Specification {
+class ParserSpec extends Specification {
   "ParserTree " should {
     "read functions " in {
-      val parser = new ParserTree
+      val parser = new Parser
       parser.execute("code2.dd")
 
       parser.funcs.keySet mustEqual Set("fibo", "main")
     }
 
     "read functions 2" in {
-      val parser = new ParserTree
+      val parser = new Parser
       parser.execute("code3.dd")
 
       parser.funcs.keySet mustEqual Set("fibo", "main")
     }
 
     "execute code 4" in {
-      val parser = new ParserTree
+      val parser = new Parser
       // no end if
-      parser.execute("code4.dd") must throwA[ParserTree.ParserSyntaxErrorException]
+      parser.execute("code4.dd") must throwA[Parser.ParserSyntaxErrorException]
     }
 
     "read code 5" in {
-      val parser = new ParserTree
+      val parser = new Parser
       parser.execute("code5.dd")
 
       parser.funcs.keySet mustEqual Set("fibo", "main", "fiboRec", "printfib")
